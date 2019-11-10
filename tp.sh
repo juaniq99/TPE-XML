33e3e6d4da5efb63b4db00911b3bc4ad5be62101
juaniq99 <52380904+juaniq99@users.noreply.github.com>
2019-11-10 16:15:30 -0300

    Update tp.sh

--- tp.sh
+++ tp.sh
@@ -11,25 +11,33 @@ error=0
 if [ $? -ne 0 ]
 then
 	echo "Error de argumento, ingrese un numero entero"
-	let error = 1
+	let error=1
+elif [ 0 -gt $1 ]
+then
+	echo "Error de argumento, su numero no es positivo"
+	let error=1
 elif [ ! -e data.xml ]
 then
 	echo "Error, falta data.xml"
 	let error=1
 elif [ ! -e metadata.xml ]
 then
-	echo "Error , falta metadata.xml"
+	echo "Error, falta metadata.xml"
 	let error=1
 elif [ ! -e xml_query.xq ]
 then
-	echo "Error , falta xml_query.xq"
+	echo "Error, falta xml_query.xq"
+	let error=1
+elif [ ! -e json_convert.xsl ]
+then
+	echo "Error, falta json_convert.xsl"
 	let error=1
 fi
 
 if [ $error -eq 0 ]
 then
 	java net.sf.saxon.Query xml_query.xq years=$1 > intermediate.xml
-	java net.sf.saxon.Transform -s:intermediate.xml -xsl:orig_json.xsl > output.json
+	java net.sf.saxon.Transform -s:intermediate.xml -xsl:json_convert.xsl > output.json
 	exit 0 
 else
 	exit 1
